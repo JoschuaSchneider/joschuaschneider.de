e56b36d8aaa44e32e7ec4dee0c37c98e144f3837
Joschua Schneider <joschua-schneider@web.de>
2019-05-24 08:44:07 +0200

    removed boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,24 +41,10 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-140227263-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
-        // Setting this parameter is optional
         anonymize: true,
-        // Setting this parameter is also optional
         respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**"],
-        // Enables Google Optimize using your container Id
-        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        //sampleRate: 5,
-        //siteSpeedSampleRate: 10,
-        //cookieDomain: "joschuaschneider.de",
+        exclude: ["/preview/**"]
       },
     },
     {
@@ -70,11 +56,8 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#c93e3e`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
